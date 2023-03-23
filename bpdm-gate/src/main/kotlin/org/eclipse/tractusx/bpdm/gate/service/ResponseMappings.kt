1d66a7d2e39a5b127c7845009c6fc2bb55b3c5d4
fabio.d.mota <fabio.d.mota@cgi.com>
2023-03-23 19:46:07 +0000

    feat(Gate): Add DTO and Mapping from model to entity

---
+++ bpdm-gate/src/main/kotlin/org/eclipse/tractusx/bpdm/gate/service/ResponseMappings.kt
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 2021,2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ******************************************************************************/
+
+package org.eclipse.tractusx.bpdm.gate.service
+
+import org.eclipse.tractusx.bpdm.common.dto.response.*
+import org.eclipse.tractusx.bpdm.common.service.toDto
+import org.eclipse.tractusx.bpdm.gate.dto.AddressGateInputRequest
+import org.eclipse.tractusx.bpdm.gate.entity.AddressGate
+
+import org.springframework.data.domain.Page
+
+
+fun <S, T> Page<S>.toDto(dtoContent: Collection<T>): PageResponse<T> {
+    return PageResponse(this.totalElements, this.totalPages, this.number, this.numberOfElements, dtoContent)
+}
+
+fun AddressGate.toAddressGateInputRequest(): AddressGateInputRequest {
+
+
+    return AddressGateInputRequest(
+        addressGate.toDto(),
+        this.externalId,
+        this.legalEntityExternalId,
+        this.siteExternalId,
+        this.bpn
+    )
+}
+fun AddressGateInputRequest.toAddressGate(): AddressGate {
+
+
+    return AddressGate(
+        this.address
+        this.externalId,
+        this.legalEntityExternalId,
+        this.siteExternalId,
+        this.bpn
+    )
+}
+
+
+
